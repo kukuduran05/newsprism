2ccdc63dc05c148f1bfe9373deaf1601e373e967
KarlaSolis723 <karla.solis@encora.com>
2021-10-08 16:05:57 -0500

    I removed all for start again the project

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-    return (
-        <div>
-            <p> Index Pages</p>
-        </div>
-    )
-}
-
-export default Index;
--- utils/api/log.ts
+++
@@ -1,9 +0,0 @@
-export const log = async (resolve, parent, args, ctx, info) => {
-    try {
-        const res = await resolve()
-        return res
-    } catch (e) {
-        console.log(e)
-    }
-
-}
