1e6bf67e04fc588e4882367284597d4ffec6ed1e
KarlaSolis723 <karla.solis@encora.com>
2021-10-08 12:26:50 -0500

    configuration

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <div>
+            <p> Index Pages</p>
+        </div>
+    )
+}
+
+export default Index;
---
+++ utils/api/resolvers.ts
@@ -0,0 +1,14 @@
+
+export const resolvers = {
+    Query: {
+        hello: (parent, args, context) => 'hi!',
+        feed: (parent, {data: {id }}, {prisma}) => prisma.feed.findUnique({where: {id}}),
+        feeds: (parent, args, {prisma}) => prisma.feed.findMany()
+    },
+    Mutation: {
+        createFeed: async (parent, {data}, {prisma, user}) => {
+            const result = await prisma.feed.create({data: {...data}})
+            return result
+        }
+    }
+}
