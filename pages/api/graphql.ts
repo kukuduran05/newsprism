b98b609df478fb50cc370634b6788b26805f0b49
KarlaSolis723 <karla.solis@encora.com>
2021-10-09 09:52:01 -0500

    Add context and middleware

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,17 +1,34 @@
 import { ApolloServer } from "apollo-server-micro";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from "../../utils/api/typeDefs";
 import { resolvers } from "../../utils/api/resolvers";
+import { applyMiddleware } from "graphql-middleware";
+import { log } from "../../utils/api/log";
+import { context } from "../../utils/api/context";
+import { permissions } from "../../utils/api/permissions";
 
+const schema = applyMiddleware(makeExecutableSchema({typeDefs, resolvers}),
+    log,
+    permissions);
 
-const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  export const config = {
+    
+export const config = {
     api: {
-      bodyParser: false
+        bodyParser: false
     }
-  };
+};
 
-export default apolloServer.createHandler({ path: '/api/graphql' });
+const handler = new ApolloServer({
+    schema
+}).createHandler({
+    path: '/api/graphql'
+});
+
+export default handler;
+// const apolloServer = new ApolloServer(applyMiddleware(makeExecutableSchema({
+//     typeDefs,
+//     resolvers,
+//     context
+//   })), log);
+
+// export default apolloServer.createHandler({ path: '/api/graphql' });
